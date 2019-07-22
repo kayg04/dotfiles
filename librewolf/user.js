7425915714c424b84da554079e40b6d04e6137ea
K Gopal Krishna <mail@kayg.org>
2019-07-22 11:54:05 +0530

    trimmed user.js since most options are already included in librewolf; added modified policies.json

---
+++ librewolf/user.js
@@ -0,0 +1,15 @@
+// Force enable hardware acceleration
+user_pref("layers.acceleration.force-enabled", true);
+
+// WebRender is automatically disabled for screens < 4K
+user_pref("gfx.webrender.all", true);
+
+// Enable accelerated azure canvas
+user_pref("gfx.canvas.azure.accelerated", true);
+
+// Do caching in RAM instead of disk
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+
+// Save session data every 5 minutes instead of every 15 seconds
+user_pref("browser.sessionstore.interval", 300000);
