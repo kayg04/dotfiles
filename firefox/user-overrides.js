74b09b82e3058dc3371bf7f990763c1d41ec21fa
K Gopal Krishna <mail@kayg.org>
2019-07-25 13:53:25 +0530

    switched to ghacks userjs and flipped required options

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,11 +1,15 @@
+/// GPU Acceleration ///
+
 // Force enable hardware acceleration
 user_pref("layers.acceleration.force-enabled", true);
-
 // WebRender is automatically disabled for screens < 4K
 user_pref("gfx.webrender.all", true);
-
 // Enable accelerated azure canvas
 user_pref("gfx.canvas.azure.accelerated", true);
+
+/// GPU Acceleration ///
+
+/// Storage ///
 
 // Do caching in RAM instead of disk
 user_pref("browser.cache.disk.enable", false);
@@ -13,3 +17,24 @@ user_pref("browser.cache.memory.enable", true);
 
 // Save session data every 5 minutes instead of every 15 seconds
 user_pref("browser.sessionstore.interval", 300000);
+
+/// Storage ///
+
+/// Search ///
+
+// Search via address bar
+user_pref("keyword.enabled", false);
+
+// Enable suggestion of searches; safe since I use SearX
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.searches", false);
+
+/// Search ///
+
+
+/// Misc ///
+
+// Disable letterboxing
+user_pref("privacy.resistFingerprinting.letterboxing", true);
+
+/// Misc ///
