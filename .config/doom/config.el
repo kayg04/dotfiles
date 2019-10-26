79facf23c82b79b9be83b43404b460f41d3decd1
K Gopal Krishna <kayg@ruri.localdomain>
2019-10-26 12:09:24 +0530

    removed: awesome, compton as i do not use them anymore
    added: packages.el -- command-log-mode to show elisp functions as
    they're being executed
    
    changed: value of org-src-preserve-indentation from t to nil by lazy
    loading

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,6 +1,7 @@
 (setq doom-theme 'doom-molokai)
-(after! org-mode
+(after! org
   (setq org-src-preserve-indentation nil))
+(setq treemacs-width 25)
 
 (setq doom-font (font-spec :family "IBM Plex Mono" :size 26 :weight 'semi-bold)
       doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
