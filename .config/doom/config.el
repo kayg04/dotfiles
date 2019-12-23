c33ec076925e85badf9cd455338aeabaefa40ce2
K Gopal Krishna <kayg@ruri.localdomain>
2019-12-23 21:36:51 +0530

    changed font, enabled lsp-ui-doc
    - increased font size to better suit the eyes
    - enabled lsp-ui-doc and let the documentation display on point instead
    of at the top or bottom
    - include the keyword in the documentation as the child frame is
    displayed directly over the cursor. I'm looking for ways to make it
    display a line below or above the cursor to increase visbility

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,20 +2,23 @@
   (setq org-src-preserve-indentation nil))
 
 (after! lsp-ui
-  (setq lsp-ui-sideline-enable nil))
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-doc-header t))
 
 (setq treemacs-width 25)
 
 (when (string= (system-name) "ruri")
   (setq doom-theme 'doom-outrun-electric)
-  (setq doom-font (font-spec :family "IBM Plex Mono" :size 28 :weight 'semi-bold)
+  (setq doom-font (font-spec :family "IBM Plex Mono" :size 30 :weight 'semi-bold)
         doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
         doom-unicode-font (font-spec :family "Input Mono Narrow" :size 28)
-        doom-big-font (font-spec :family "IBM Plex Mono" :size 40 :weight 'semi-bold)))
+        doom-big-font (font-spec :family "IBM Plex Mono" :size 42 :weight 'semi-bold)))
 
 (when (string= (system-name) "nana")
   (setq doom-theme 'doom-dracula)
-  (setq doom-font (font-spec :family "SF Mono" :size 16 :weight 'semi-bold)
+  (setq doom-font (font-spec :family "SF Mono" :size 18 :weight 'semi-bold)
         doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
         doom-unicode-font (font-spec :family "Input Mono Narrow" :size 16)
-        doom-big-font (font-spec :family "SF Mono" :size 28 :weight 'semi-bold)))
+        doom-big-font (font-spec :family "SF Mono" :size 30 :weight 'semi-bold)))
