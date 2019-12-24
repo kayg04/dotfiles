4424d5a2efb538982964b72fe2cd9537a52c0f4e
K Gopal Krishna <kayg@ruri.localdomain>
2019-12-24 20:25:57 +0530

    added more info and increased font size for nana

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -5,20 +5,27 @@
   (setq lsp-ui-sideline-enable nil)
   (setq lsp-ui-doc-enable t)
   (setq lsp-ui-doc-position 'at-point)
-  (setq lsp-ui-doc-header t))
+  (setq lsp-ui-doc-header t)
+  (setq lsp-ui-doc-max-height 6)
+  (setq lsp-ui-doc-max-width 54))
+
+(add-hook 'lsp-ui-doc-frame-hook
+          (lambda (frame _w)
+            (set-face-attribute 'default frame :font "Fira Mono" :height 132)))
 
 (setq treemacs-width 25)
+(setq evil-escape-unordered-key-sequence t)
 
 (when (string= (system-name) "ruri")
   (setq doom-theme 'doom-outrun-electric)
-  (setq doom-font (font-spec :family "IBM Plex Mono" :size 30 :weight 'semi-bold)
+  (setq doom-font (font-spec :family "IBM Plex Mono" :size 28 :weight 'semi-bold)
         doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
-        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 28)
-        doom-big-font (font-spec :family "IBM Plex Mono" :size 42 :weight 'semi-bold)))
+        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 26)
+        doom-big-font (font-spec :family "IBM Plex Mono" :size 44 :weight 'semi-bold)))
 
 (when (string= (system-name) "nana")
   (setq doom-theme 'doom-dracula)
-  (setq doom-font (font-spec :family "SF Mono" :size 18 :weight 'semi-bold)
+  (setq doom-font (font-spec :family "SF Mono" :size 20 :weight 'semi-bold)
         doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
-        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 16)
-        doom-big-font (font-spec :family "SF Mono" :size 30 :weight 'semi-bold)))
+        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 18)
+        doom-big-font (font-spec :family "SF Mono" :size 34 :weight 'semi-bold)))
