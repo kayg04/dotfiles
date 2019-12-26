e552e8204cd128b12c9951923256a66b9ee76335
K Gopal Krishna <kayg@ruri.localdomain>
2019-12-26 09:17:49 +0530

    ui changes around fonts and theme

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -10,23 +10,30 @@
   (setq lsp-ui-doc-max-height 6)
   (setq lsp-ui-doc-max-width 54))
 
+;; Font changes
 (add-hook 'lsp-ui-doc-frame-hook
           (lambda (frame _w)
             (set-face-attribute 'default frame :font "Fira Mono" :height 132)))
+
+(defface my-prog-mode-default-face
+  '((t (:inherit default :family "Iosevka")))
+  "Programming Mode Default Face")
+(add-hook! 'prog-mode-hook (face-remap-add-relative 'default 'my-prog-mode-default-face))
 
 (setq treemacs-width 25)
 (setq evil-escape-unordered-key-sequence t)
 
 (when (string= (system-name) "ruri")
   (setq doom-theme 'doom-outrun-electric)
+  (setq doom-outrun-electric-comment-bg t)
   (setq doom-font (font-spec :family "IBM Plex Mono" :size 28 :weight 'semi-bold)
-        doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
-        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 26)
-        doom-big-font (font-spec :family "IBM Plex Mono" :size 44 :weight 'semi-bold)))
+        doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 26 :weight 'semi-bold)
+        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 28)
+        doom-big-font (font-spec :family "IBM Plex Mono" :size 44 :weight 'bold)))
 
 (when (string= (system-name) "nana")
   (setq doom-theme 'doom-dracula)
-  (setq doom-font (font-spec :family "SF Mono" :size 22 :weight 'semi-bold)
-        doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
-        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 18)
+  (setq doom-font (font-spec :family "SF Mono" :size 20 :weight 'semi-bold)
+        doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 18 :weight 'semi-bold)
+        doom-unicode-font (font-spec :family "Input Mono Narrow" :size 20)
         doom-big-font (font-spec :family "SF Mono" :size 36 :weight 'semi-bold)))
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -34,7 +34,8 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       ;;pretty-code       ; replace bits of code with pretty symbols
+       (pretty-code       ; replace bits of code with pretty symbols
+        +iosevka)
        tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
@@ -42,6 +43,8 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
+
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
