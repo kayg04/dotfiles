34b52210f312df27f2cf982d35eaa2c3ee75f5e6
K Gopal Krishna <kayg@ruri.localdomain>
2020-01-02 21:59:19 +0530

    switched to GNU stow
    - stop reinventing the wheel

--- .mozilla/firefox/materialfox.js
+++
@@ -1,9 +0,0 @@
-/// MaterialFox ///
-
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("browser.tabs.tabClipWidth", 83);
-user_pref("materialFox.reduceTabOverflow", true);
-user_pref("security.insecure_connection_text.enabled", true);
-
-/// MaterialFox ///
